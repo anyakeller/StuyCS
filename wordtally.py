008830bc1bfdecc816322796285d0eee8ac79cd3
Anya Keller <anyaisanya@gmail.com>
2015-05-11 09:06:28 -0400

    YAY

---
+++ wordtally.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python 
+# === LINE BELOW IS MAGIC TOO === 
+print 'Content-Type: text/html\n' 
+ 
+# === MORE MAGIC === 
+print ' ' #Sha-Bang!!! 
+print '<html>' 
+print '<title> Word Count    </title>'   #thanks to Richard Lin's template 
+print '<head> <h1> Word Count   </h1> </head>'    
+
+#WRITE CODESTUFF HERE 
+
+print '<table>'
+print "<tr><td>a</td><td> 1</td></tr>"
+print "<tr><td>lkj</td><td> 1</td></tr>"
+print "<tr><td>potato</td><td> 1</td></tr>"
+print "<tr><td>potatoes</td><td> 1</td></tr>"
+print "<tr><td>is</td><td> 1</td></tr>"
+print "<tr><td>laskdjfl</td><td> 1</td></tr>"
+print "<tr><td>derp</td><td> 1</td></tr>"
+print "<tr><td>hi</td><td> 1</td></tr>"
+print "<tr><td>derpity</td><td> 1</td></tr>"
+print "<tr><td>asdf</td><td> 1</td></tr>"
+print "<tr><td>anya</td><td> 1</td></tr>"
+print '</table>' 
+print '</html>' 
